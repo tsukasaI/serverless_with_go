9aa701815258af220c88289bbc780d9bb6985873
tsukasa inoue <inoue.tsukasa.4@gmail.com>
2023-11-11 22:21:21 +0900

    use env

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"context"
 	"log"
+	"time"
+
+	env "github.com/caarlos0/env/v10"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -10,13 +13,31 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type config struct {
+	Home         string         `env:"HOME"`
+	Port         int            `env:"PORT" envDefault:"3000"`
+	Password     string         `env:"PASSWORD,unset"`
+	IsProduction bool           `env:"PRODUCTION"`
+	Duration     time.Duration  `env:"DURATION"`
+	Hosts        []string       `env:"HOSTS" envSeparator:":"`
+	TempFolder   string         `env:"TEMP_FOLDER,expand" envDefault:"${HOME}/tmp"`
+	StringInts   map[string]int `env:"MAP_STRING_INT"`
+	APP          string         `env:"APP"`
+}
+
 var ginLambda *ginadapter.GinLambda
 
 func init() {
 	// stdout and stderr are sent to AWS CloudWatch Logs
 	log.Printf("Gin cold start")
+
+	cfg := config{}
+	if err := env.Parse(&cfg); err != nil {
+		log.Printf("%+v\n", err)
+	}
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
+		log.Printf("\n\n===\n\n%+v\n\n", cfg)
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
